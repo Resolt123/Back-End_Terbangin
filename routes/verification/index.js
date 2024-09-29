5a3488c23ee6a18df5cd29e7256bd22324367a9d
Rionase <nicholasssputra@gmail.com>
2024-09-29 15:42:32 +0700

    REFACTOR : Remove generate OTP SMS API

--- routes/verification/index.js
+++ routes/verification/index.js
@@ -4,7 +4,7 @@ const otpController = require("../../controllers/otp");
 const linkController = require("../../controllers/link");
 
 router.post("/generate-otp-email", otpController.generateOTPEmail);
-router.post("/generate-otp-sms", otpController.generateOTPSMS);
+// router.post("/generate-otp-sms", otpController.generateOTPSMS);
 router.post("/verify-otp", otpController.verifyOTP);
 router.post("/generate-link", linkController.generateLink);
 router.get("/verify-link", linkController.verifyLink);
